645a546a241bcebbee7b487d4a10e2c986ff34cc
PaigePonzeka <paigepon@gmail.com>
2015-03-06 15:07:47 -0500

    Merged files

---
+++ config/routes.rb.BASE.2875.rb
@@ -0,0 +1,58 @@
+Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  resources :games
+  resources :fields
+  resources :committees
+  resources :board_members
+  resources :hallof_famers
+  resources :admins
+  #resources :ratings
+  resources :teams
+  resources :profiles
+  resources :teams_sponsors, :only => [:index, :show]
+  resources :sponsors
+  resources :seasons, :only => [:index, :show]
+  resources :divisions, :only => [:index, :show]
+  resources :teams, :only => [:index, :show]
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get '/home', to:'static_pages#home'
+  # sessions
+  match '/signup',  to: 'profiles#new', via: 'get'
+  #match '/help',    to: 'static_pages#help',    via: 'get'
+  #match '/about',   to: 'static_pages#about',   via: 'get'
+  #match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signin',  to: 'sessions#new', via: 'get'
+  get '/signout', to: 'sessions#destroy', via: 'delete'
+  get '/teams_by_season', to: 'teams#get_teams_by_season'
+
+  get '/welcome', to: 'welcome#index'
+  #teamsnap api login
+  get '/login', to: 'welcome#login'
+  get '/teamsnaplogout', to: 'welcome#logout'
+  get '/teamssnap/:teamId/:rosterId', to: 'welcome#team', as: 'rosterId'
+  get '/ranking/:teamId/:rosterId/:playerId', to: 'welcome#ranking', as: 'playerId'
+  post '/teamsnaplogin', to: 'welcome#teamsnaplogin'
+
+  # website iframes
+  get '/showallsponsors', to: 'sponsors#all_sponsors'
+  get '/sponsorscarousel', to: 'sponsors#sponsor_carousel'
+  get '/showallhof', to: 'hallof_famers#all_hof'
+  get '/showallboard', to: 'board_members#all_board'
+  get '/showallcommittee', to: 'committees#all_committee'
+  get '/showfields', to: 'fields#show_map'
+  get '/loadsidebar', to: 'welcome#basl_sidebar'
+  get '/loadcalendar', to: 'welcome#load_calendar'
+  get '/getleaguesponsors', to: 'sponsors#league_sponsors'
+
+  #helpers
+  post '/fields/set_all', to: 'fields#set_all'
+
+  #admin panel
+  get '/admin/home', to: 'admins#home'
+
+  #errors
+  get '/403', to: 'welcome#error403'
+
+end
