b9c6d7decf9250c37707195cc126bbca0efabc9d
Paige Ponzeka <pponzeka@yext.com>
2015-03-12 18:23:13 -0400

    Updating migration

--- db/migrate/20150225235943_fix_games_table.rb
+++ db/migrate/20150225235943_fix_games_table.rb
@@ -1,6 +1,6 @@
 class FixGamesTable < ActiveRecord::Migration
   def change
     remove_column :games, :integer
-    change_column :games, :home_team_id, :integer
+    change_column :games, :home_team_id, 'integer USING CAST(column_name AS integer)'
   end
 end
