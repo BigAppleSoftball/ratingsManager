fa4e25e46828e6ff26e387336080dca6befd881d
Paige Ponzeka <pponzeka@yext.com>
2015-04-29 18:12:52 -0400

    Using Eager load on games controller to speed up the load time. much faster now

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,7 @@ class GamesController < ApplicationController
   # GET /games
   # GET /games.json
   def index
-    @games = Game.all
+    @games = Game.eager_load(:home_team, :away_team, :field).all
   end
 
   # GET /games/1
