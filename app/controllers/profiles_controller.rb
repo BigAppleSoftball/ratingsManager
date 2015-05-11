2051fd9268deb33240d5f7f61bcb3b52252bc232
Paige Ponzeka <pponzeka@yext.com>
2015-05-11 19:33:52 -0400

    Cleaning up the Profiles information from the import

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -74,7 +74,7 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:profile_code, :first_name, :last_name, :email, :display_name, :player_number, :gender, :shirt_size, :address, :state, :zip, :phone, :position, :dob, :team_id, :long_image_url, :password, :password_confirmation, :is_admin, :permissions, :address2, :city)
+      params.require(:profile).permit(:profile_code, :first_name, :last_name, :email, :display_name, :player_number, :gender, :shirt_size, :address, :state, :zip, :phone, :position, :dob, :team_id, :long_image_url, :password, :password_confirmation, :is_admin, :permissions, :address2, :city, :is_pickup_player)
     end
 
     def sort_column
