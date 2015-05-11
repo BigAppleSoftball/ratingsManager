ffac24201f7081df5f3f8fba4abb429bdfdb94c2
PaigePonzeka <paigepon@gmail.com>
2015-05-11 09:07:56 -0400

    deleteing a game nullifys the game

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ActiveRecord::Base
   belongs_to :division
-  has_one :home_games, :class_name => "Game", :foreign_key => 'home_team_id'
-  has_one :away_games, :class_name => "Game", :foreign_key => 'away_team_id'
+  has_one :home_games, :class_name => "Game", :foreign_key => 'home_team_id', dependent: :nullify
+  has_one :away_games, :class_name => "Game", :foreign_key => 'away_team_id', dependent: :nullify
   has_many :sponsors, :through => :teams_sponsor
   has_many :profiles
   has_many :rosters, dependent: :destroy
