f5a1ea34cb43f87659b60a076036d89c2ea30877
PaigePonzeka <paigepon@gmail.com>
2015-05-17 08:30:07 -0400

    Adding the abiliy to merge in player profiles, with a redirect and backend code Closes #68

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -22,6 +22,7 @@
 
   MergeProfile.prototype.init = function() {
     this.bindMergeProfileSelector();
+    this.bindRunMergeBtn();
   };
 
   /**
@@ -45,12 +46,76 @@
       dataType:'JSON',
       data: {profile_id: profileId},
       success: function(data){
-        console.log(data);
         if (data && data.html) {
           $('.js-merge-profile-details').html(data.html);
         }
       }
     });
+  };
+
+  /**
+   *  Binds the event listener for running the merge
+   */
+  MergeProfile.prototype.bindRunMergeBtn = function() {
+    var self = this;
+
+    $('.js-run-merge').on('click', function(e){
+      e.preventDefault();
+      var $profile1 = $('.js-profile-1'),
+          $profile1Details = $profile1.find('.js-profile-details'),
+          profile1_id = $profile1Details.data('id'),
+          profile1_name = $profile1Details.data('name'),
+          $profile2 = $('.js-profile-2'),
+          $profile2Details = $profile2.find('.js-profile-details'),
+          profile2_id = $profile2Details.data('id'),
+          profile2_name = $profile2Details.data('name');
+
+      if (!profile1_id || !profile2_id) {
+        self.showMergeError('Need two Profiles to run merge');
+        return;
+      }
+
+      if (profile1_id == profile2_id) {
+        self.showMergeError('Cannot merge two of the same profiles');
+        return;
+      }
+
+      var isConfirmed = confirm('Are you sure you want to delete and merge ' + profile1_name + ' into ' + profile2_name + '. This will result in the deletion of ' + profile1_name + '(' + profile1_id  + ') and all rosters, committees, and board memberships will be merged into ' + profile2_name + '.');
+
+      if (isConfirmed) {
+        // run ajax to run the merge
+        var postRequest = {
+          'profile1_id' : profile1_id,
+          'profile2_id' : profile2_id
+        }; 
+
+        $.ajax({
+          url: 'merge',
+          type: 'POST',
+          dataType:'JSON',
+          data: postRequest,
+          success: function(data){
+            if (data && data.success) {
+              // show a message to indicate the profile was deleted
+              $.toaster({ priority : 'success', title : 'Success!', message : data.success})
+              // update the UI to see the removed psot
+              if (data.profile_link) {
+                window.location.replace(data.profile_link);
+              }
+            } else {
+              self.showMergeError(data.error);
+            }
+          }
+        });
+      }
+    });
+  };
+
+  MergeProfile.prototype.showMergeError = function(msg) {
+    if (!msg) {
+      msg = 'Something Went wrong please refresh and try again';
+    }
+    $.toaster({ priority : 'danger', title : 'Error!', message : msg});
   };
 
   var profile = new Profile();
