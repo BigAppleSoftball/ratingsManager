babbb8cc32b312292c654174120bf9bbe9d56a27
PaigePonzeka <paigepon@gmail.com>
2015-06-04 08:59:52 -0400

    Adding the ability to generate a schedule

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -4,63 +4,105 @@ class SchedulesController < ApplicationController
   def index
     @division = Division.eager_load(:teams).find(52)
     teams = ["team1", "team2", "team3", "team4", "team5", "team6", 'teams7', 'teams8']
-    generate_games(teams)
+    @generated_weeks = generate_schedule(@division.teams.pluck('teams.name, teams.id'), 10)
+    render
   end
 
 
   private
-    def generate_games(teams)
-      init_games(teams)
+
+    def generate_schedule(teams, totalGames)
+      matchups = init_games(teams)
+      weeks = Array.new
+      for week_count in 1.upto(totalGames)
+        week = Hash.new
+        week[:count] = week_count
+        week[:games] = get_week_games(matchups)
+        weeks.push(week)
+        matchups = shift_teams(matchups)
+      end
+      weeks
     end
+
+    #
+    #
+    # Initialize the default layout of the games
+    # i.e. 
+    # [0]
+    #   Team1
+    #   Team2
+    #   Team3
+    #   Team4
+    #   
+    # [1]
+    #   Team5
+    #   Team6
+    #   Team7
+    #   Team8
+    #
     def init_games(teams)
-      require 'matrix'
       gamesPerWeek = teams.size/2
-      ap gamesPerWeek
-      vmatchup = []
-      hmatchup = []
+      matchups = Hash.new
+      matchups[0] = []
+      matchups[1] = []
       teamCount = 0
       for x in 0..1
         for y in 0..(gamesPerWeek-1)
-          if (x == 0)
-            vmatchup[y] = teams[teamCount]
-          elsif (x == 1)
-            hmatchup[y] = teams[teamCount]
-          end
+          matchups[x][y] = teams[teamCount]
           teamCount+=1
         end
       end
-      ap vmatchup
-      ap hmatchup
-      ap get_week_games(vmatchup, hmatchup)
-      shift_games(vmatchup, hmatchup)
+      matchups
     end
 
-    def shift_games(visitors, home)
-      numOfTeams = visitors.size
-      vTemp = visitors[1]
+    #
+    #
+    # Shift the teams over for the next games
+    # i.e.
+    # [0] 
+    #   Team1
+    #   Team3
+    #   Team4
+    #   Team8
+    #   
+    # [2]
+    #   Team2
+    #   Team5
+    #   Team6
+    #   Team7
+    #
+    def shift_teams(matchups)
+      numOfTeams = matchups[0].size
+      vTemp = matchups[0][1]
+
+      # shift all the left column teams up
+      # 0,2 --> 0,1, 0,3 -> 02
       for i in 2..(numOfTeams - 1)
-        visitors[i - 1] = visitors[i] 
+        matchups[0][i - 1] = matchups[0][i] 
       end
+      # the last left column teams = the last right column team
+      matchups[0][numOfTeams - 1] = matchups[1][numOfTeams - 1]
 
-      visitors[i] = home[i]
-
-      #1 = tem1 --> 2 2 --> 3 3---> 4
-      for x in (numOfTeams-1)..1
-        home[x] = home[x - 1]
+      # shift all the right column games up
+      # 1,2 --> 1,3 , 1,2 --> 1,1, 1, 0 --> 1,1
+      for x in (numOfTeams - 1 ).downto(0)
+        matchups[1][x] = matchups[1][x - 1]
       end
-      home[0] = vTemp
-
-      ap visitors
-      ap home
+      matchups[1][0] = vTemp
+    
+      matchups
 
     end
 
-    def get_week_games(visitors, home)
-      numOfGames = visitors.size
+    def get_week_games(matchups)
+      numOfGames = matchups[0].size
       games = []
       # todo should make sure home and visitors are the same size
       for x in 0..(numOfGames - 1) 
-        games.push("#{visitors[x]} vs. #{home[x]}")
+        game = Hash.new
+        game[:visitor] = matchups[0][x]
+        game[:home] = matchups[1][x]
+        games.push(game)
       end
       games
     end
