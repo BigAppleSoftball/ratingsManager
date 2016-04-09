d46d7145476e1195f2befae219fc8b3fc2c6a1cd
PaigePonzeka <paigeponzeka@yext.com>
2016-04-08 20:47:08 -0400

    Create one-to-many relationship between roles and permissions

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,2 +1,4 @@
 class Permission < ActiveRecord::Base
+  has_many :roles, :through => :roles_permission
+  has_many :roles_permission, dependent: :destroy
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,4 @@
 class Role < ActiveRecord::Base
+  has_many :permissions, :through => :roles_permission
+  has_many :roles_permission, dependent: :destroy
 end
